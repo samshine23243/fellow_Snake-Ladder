82ea250ee991650f8d2d9c480ce8b93a0c0bac27
samshine <sameershinde23243@gmail.com>
2021-03-11 10:19:39 +0530

    [sameer] start msg code added

--- Game.java
+++ Game.java
@@ -1,9 +1,18 @@
 
 public class Game {
+	void startmsg()
+	{
+		System.out.println("Player A & B");
+		System.out.println("A:0 & B:0");
+		System.out.println("lets play!!!");
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("!!!! Snake & ladder Game !!!!");
+		Game obj=new Game();
+		obj.startmsg();
 
 	}
 
